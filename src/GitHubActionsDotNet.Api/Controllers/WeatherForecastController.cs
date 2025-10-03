d76690a97f079d21c657de7bc16245e686729529
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-03 23:03:53 +0500

    More weather summaries added.

--- src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
+++ src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
@@ -8,6 +8,16 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries =
     [
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching",
         "Cloudy"
     ];
 
