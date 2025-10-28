7644c0af5cc3ff635794b0909f9c1ab772877715
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-10-28 15:06:39 +0500

    Summary updated. (#35)

--- src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
+++ src/GitHubActionsDotNet.Api/Controllers/WeatherForecastController.cs
@@ -8,17 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries =
     [
-        "Freezing",
-        "Bracing",
-        "Chilly",
-        "Cool",
-        "Mild",
-        "Warm",
-        "Balmy",
-        "Hot",
-        "Sweltering",
-        "Scorching",
-        "Cloudy"
+        "Sunny"
     ];
 
     [HttpGet(Name = "GetWeatherForecast")]
